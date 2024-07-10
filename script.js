4b9f247470517ceb1eb4e6c513a470d56dd541d8
teia <tmyfrncnn@gmail.com>
2024-07-10 10:49:19 +0800

    feat: enhance 'Add Income' feature to update dashboard value progressively.

--- script.js
+++ script.js
@@ -86,9 +86,15 @@
 
   // for add income (trial1) update: not trial anymore bec it wokred hehe UwU
 
-  var addIncomeModal = document.getElementById("addIncomeModal");
   var toggleAddIncome = document.getElementById("toggleAddIncome");
   var IncomeCancelBtn = document.getElementById("IncomeCancelBtn");
+
+  // new code for when cost will reflect on dashboard upon putting the value on input field
+
+  var addIncomeModal = document.getElementById("addIncomeModal");
+  var incomeInputField = document.getElementById ("incomeInputField");
+  var IncomeEnterBtn = document.getElementById ("IncomeEnterBtn");
+  var totalIncome = document.getElementById ("totalIncome")
 
   toggleAddIncome.onclick = function () {
     addIncomeModal.style.display = "flex";
@@ -97,3 +103,32 @@
   IncomeCancelBtn.onclick = function () {
     addIncomeModal.style.display = "none";
   };
+
+// new code for when cost will reflect on dashboard upon putting the value on input field (1)
+
+IncomeEnterBtn.onclick = function () {
+
+  // nag make us another variable which is 'cost' and that 'cost's' value is: var cost = x (y.z), where z is a string
+  var cost = parseFloat(incomeInputField.value);
+
+  // after making another variable, now we make an if else condition to achieve what we wanted to achieve
+
+  if (!isNaN(cost) && cost > 0) {
+
+    // here we can see we made another variable to use sa next condition sa if else: var currentIncome = x (y.z), where z is another kind of value
+      var currentIncome = parseFloat(totalIncome.textContent);
+
+      totalIncome.textContent = (currentIncome + cost).toFixed(2);
+
+      addIncomeModal.style.display = "none";
+  } else {
+      alert("Please enter a valid cost.");
+  }
+};
+
+// ^^ nag hatag ra condition on what is going on if u click
+// the enter button. bale, nag hatag sha condition where if
+// you add a number sa input field, mureflect sa dashboard
+// ang value. and that mag increase na dayn ang number ana
+// because of this code:
+// totalIncome.textContent = (currentIncome + cost)
