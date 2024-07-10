4b8520f9310dcae0fd10421f95b48ff5a25f5359
teia <tmyfrncnn@gmail.com>
2024-07-10 13:25:19 +0800

    feat: add new and update existing line in html and js file

---
+++ code notes.js
@@ -0,0 +1,9 @@
+// code for when you want left click button to work
+IncomeEnterBtn.onclick = function () {
+
+};
+
+// code for when you want keybord 'enter' to work
+incomeInputField.addEventListener("keydown", function (event) {
+    
+  });
